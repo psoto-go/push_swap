b6a6c31f3cae076be3b9e4aacaff7bcb94805dd3
psoto-go <pabletesglez@gmail.com>
2021-12-14 15:43:18 +0100

    add

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: psoto-go <psoto-go@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 15:57:02 by psoto-go          #+#    #+#             */
-/*   Updated: 2021/12/10 13:28:57 by psoto-go         ###   ########.fr       */
+/*   Updated: 2021/12/14 13:51:13 by psoto-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,11 @@
 # define MAX_INT 2147483647
 # define MIN_INT -2147483648
 
-struct nodo
-{
-	int				number;
-	struct nodo		*next;
-};
+// typedef struct nodo
+// {
+// 	int				number;
+// 	struct nodo		*next;
+// }t_nodo;
 
 struct nodo *superior = NULL;
 
--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,45 +6,47 @@
 /*   By: psoto-go <psoto-go@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 15:56:45 by psoto-go          #+#    #+#             */
-/*   Updated: 2021/12/10 17:47:39 by psoto-go         ###   ########.fr       */
+/*   Updated: 2021/12/14 15:40:16 by psoto-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
 // Operación push
-void agregar(int number) {
-    // printf("Agregando %d\n", number);
-    // El que se agregará; reservamos memoria
-    struct nodo *nuevoNodo;
-	
-	nuevoNodo = malloc(sizeof(struct nodo));
+void	agregar(int content, t_list *lista) {
+	t_list *current;
+	lista = malloc(sizeof(t_list));
+	if (!lista)
+		return ;
+	current = lista;
     // Le ponemos el dato
-    nuevoNodo->number = number;
+    current->content = content;
+	printf("%daasfasdf\n", current->content);
     // Y ahora el nuevo nodo es el superior, y su siguiente
     // es el que antes era superior
-    nuevoNodo->next = superior;
-    superior = nuevoNodo;
+    current = current->next;
 }
 
 void imprimir(void) {
+	t_list *current;
+
+	current = NULL;
     printf("Imprimiendo...\n");
-    struct nodo *temporal = superior;
-    while (temporal != NULL) {
-        printf("%d\n", temporal->number);
-        temporal = temporal->next;
+    while (current != NULL) {
+        printf("%dp\n", current->content);
+        current = current->next;
     }
 }
 
-void	tamanio(void){
-    int contador = 0;
-    struct nodo *temporal = superior;
-    while (temporal != NULL) {
-		contador++;
-        temporal = temporal->next;
-    }
-	printf("Contador es: %d", contador);
-}
+// void	tamanio(void){
+//     int contador = 0;
+//     struct nodo *temporal = superior;
+//     while (temporal != NULL) {
+// 		contador++;
+//         temporal = temporal->next;
+//     }
+// 	printf("Contador es: %d", contador);
+// }
 
 void	ft_error(void)
 {
@@ -74,7 +76,51 @@ int		checkParams(char *argv)
 	return (n);
 }
 
-void	get_numbers(char *argv)
+
+int		ft_arraylen(char **str)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while(str[i])
+	{
+		j++;
+		i++;
+	}
+	return(j);
+}
+
+// int 	repes(char **aux)
+// {
+// 	int i;
+// 	int flag;
+// 	int j;
+
+// 	i = 0;
+// 	flag = 0;
+// 	j = 0;
+// 	while (aux[i] != '\0')
+// 	{
+// 		j = 0;
+// 		while (aux[j] != '\0')
+// 		{
+// 			printf("%s I\n", aux[i]);
+// 			printf("%s J\n", aux[j]);
+// 			j++;
+// 		}
+// 		i++;
+// 	}
+// 	return(flag);
+// }
+
+int		check_nums()
+{
+	return(0);
+}
+
+void	get_contents(char *argv, t_list *nodo)
 {
 	char	**aux;
 	long	num;
@@ -82,10 +128,11 @@ void	get_numbers(char *argv)
 
 	aux = ft_split(argv, ' ');
 	i = 0;
+	// repes(aux);
 	while (aux[i] != '\0')
 	{
 		num = checkParams(aux[i]);
-		agregar(num);
+		agregar(num, nodo);
 		free(aux[i]);
 		i++;
 	}
@@ -94,6 +141,7 @@ void	get_numbers(char *argv)
 
 int			main(int argc, char **argv)
 {
+	t_list nuevoNodo;
 	int i;
 
 	i = 1;
@@ -103,10 +151,10 @@ int			main(int argc, char **argv)
 	// 	ft_error();
 	while(i < argc)
 	{
-		get_numbers(argv[i]);
+		get_contents(argv[i], &nuevoNodo);
 		i++;
 	}
-	// imprimir();
+	imprimir(&nuevoNodo);
 	// tamanio();
 	// printf("\n%d", ft_atoi("a2121adf"));
 	// system("leaks push_swap");
