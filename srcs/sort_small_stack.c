700da70e695aeaf27eda75f67f891067bc1f25a9
Pablo Soto <pabletesglez@gmail.com>
2022-01-02 18:24:22 +0100

    Fin 5

--- srcs/sort_small_stack.c
+++ srcs/sort_small_stack.c
@@ -6,7 +6,7 @@
 /*   By: psoto-go <psoto-go@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/22 19:31:49 by psoto-go          #+#    #+#             */
-/*   Updated: 2021/12/31 03:33:31 by psoto-go         ###   ########.fr       */
+/*   Updated: 2022/01/02 18:22:54 by psoto-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,25 +38,20 @@ void	sort_three(t_list **stack_a)
 		swap(stack_a, 0, 'a');
 }
 
-void	sort_five(t_list **stack_a, t_list **stack_b)
+void	sort_four(t_list **stack_a)
 {
 	int num1;
 	int num2;
 	int num3;
 	int num4;
 
-	push_b(stack_a, stack_b);
-	push_b(stack_a, stack_b);
-	while (!sorted(stack_a, 3))
-			sort_three(stack_a);
-	push_a(stack_a, stack_b);
 	num1 = *(int *)(*stack_a)->content;
 	num2 = *(int *)(*stack_a)->next->content;
 	num3 = *(int *)(*stack_a)->next->next->content;
 	num4 = *(int *)(*stack_a)->next->next->next->content;
 	if (num1 > num2 && num1 < num3)
 		swap(stack_a, 0, 'a');
-	else if (num1 < num4 && num1 > num3)
+	else if (num1 > num3 && num1 < num4)
 	{
 		reverse_rotate(stack_a, 0 ,'a');
 		swap(stack_a, 0, 'a');
@@ -65,9 +60,61 @@ void	sort_five(t_list **stack_a, t_list **stack_b)
 	}
 	else if (num1 > num4)
 		rotate(stack_a, 0 , 'a');
-	push_a(stack_a, stack_b);
-	
+}
 
+void	sort_five_ifs(t_list **stack_a)
+{
+	int num1;
+	int num2;
+	int num3;
+	int num4;
+	int num5;
+
+	num1 = *(int *)(*stack_a)->content;
+	num2 = *(int *)(*stack_a)->next->content;
+	num3 = *(int *)(*stack_a)->next->next->content;
+	num4 = *(int *)(*stack_a)->next->next->next->content;
+	num5 = *(int *)(*stack_a)->next->next->next->next->content;
+
+	if (num1 > num2 && num1 < num3)
+		swap(stack_a, 0, 'a');
+	else if (num1 > num3 && num1 < num4)
+	{
+		reverse_rotate(stack_a, 0 ,'a');
+		swap(stack_a, 0, 'a');
+		reverse_rotate(stack_a, 0 ,'a');
+		swap(stack_a, 0, 'a');
+		reverse_rotate(stack_a, 0 ,'a');
+		reverse_rotate(stack_a, 0 ,'a');
+		reverse_rotate(stack_a, 0 ,'a');
+	}
+	else if (num1 > num4 && num1 < num5)
+	{
+		reverse_rotate(stack_a, 0 ,'a');
+		swap(stack_a, 0, 'a');
+		reverse_rotate(stack_a, 0 ,'a');
+		reverse_rotate(stack_a, 0 ,'a');
+		reverse_rotate(stack_a, 0 ,'a');
+	}
+	else if (num1 > num4)
+		rotate(stack_a, 0 , 'a');
+	
+}
+
+
+
+void	sort_five(t_list **stack_a, t_list **stack_b)
+{
+	push_b(stack_a, stack_b);
+	push_b(stack_a, stack_b);
+	while (!sorted(stack_a, 3))
+		sort_three(stack_a);
+	push_a(stack_a, stack_b);
+	while (!sorted(stack_a, 4))
+		sort_four(stack_a);
+	push_a(stack_a, stack_b);
+	while (!sorted(stack_a, 5))
+		sort_five_ifs(stack_a);
 }
 
 void	sort_small_stack(t_list **stack_a, t_list ** stack_b, int len)
